9909dafe2e3215557bdc8c4522a81599848195e2
goodym <11184629@qq.com>
2021-02-21 19:05:05 +0800

    修改文件

--- moyu-modules/moyu-edu/src/main/java/cn/xueden/edu/controller/front/EduSearchController.java
+++ moyu-modules/moyu-edu/src/main/java/cn/xueden/edu/controller/front/EduSearchController.java
@@ -3,8 +3,11 @@ package cn.xueden.edu.controller.front;
 
 import cn.xueden.common.core.edu.domain.EduCourse;
 import cn.xueden.common.core.edu.domain.EduKeyword;
+import cn.xueden.common.core.edu.vo.EduMemberVO;
 import cn.xueden.common.core.edu.vo.PageVO;
 import cn.xueden.common.core.utils.RestResponse;
+import cn.xueden.common.redis.service.RedisService;
+import cn.xueden.common.security.service.TokenService;
 import cn.xueden.edu.service.IEduCourseService;
 import cn.xueden.edu.service.IEduKeywordService;
 import cn.xueden.search.domain.CourseESItem;
@@ -18,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,18 +43,25 @@ public class EduSearchController {
     @Autowired
     private IEduKeywordService keywordService;
 
+    @Autowired
+    private TokenService tokenService;
+
     @ApiOperation(value = "前台快速搜索", notes = "前台快速搜索")
     @PostMapping("/getQuickSearch/{key}")
     public RestResponse getQuickSearch(@PathVariable String key,
-                                       @RequestBody String pages) {
+                                       @RequestBody String pages,
+                                       HttpServletRequest request) {
         Integer page = Integer.parseInt(JSON.parseObject(pages).get("page").toString());
         Integer size = Integer.parseInt(JSON.parseObject(pages).get("size").toString());
+        String token = request.getHeader("Authorization");
         if(page==null){
             page =1;
         }
         if(size==null){
             size =2;
         }
+        //根据token,获取登录会员信息
+        EduMemberVO eduMemberToken = tokenService.getLoginMember();
 
         Page<CourseESItem> searchs=courseService.getQuickSearch(key,page,size);
         List<CourseESItem>  esItems = searchs.getContent();
@@ -58,6 +69,8 @@ public class EduSearchController {
         Map<String,Object> result = new HashMap<>();
         result.put("esItems",esItems);
         result.put("total",total);
+
+
 
         //获取关键词
         QueryWrapper<EduKeyword> queryWrapper = new QueryWrapper<>();
@@ -70,6 +83,14 @@ public class EduSearchController {
             eduKeyword.setNum(1);
             eduKeyword.setName(key);
         }
+        if(eduMemberToken==null){
+            eduKeyword.setUpdateId(0l);
+            eduKeyword.setCreateId(0l);
+        }else {
+            eduKeyword.setUpdateId(eduMemberToken.getId());
+            eduKeyword.setCreateId(eduMemberToken.getId());
+        }
+
         keywordService.saveOrUpdate(eduKeyword);
 
       return RestResponse.success().setData(result).setCode(200);
